4d65b40e0f6b00200cca1aa59a18a3fa9b519234
Aliya Banu <aliya26205@gmail.com>
2026-06-25 21:46:18 +0530

    change added

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,11 @@
 // ===============================
-// PriceWatch - Authentication
+// PriceWatch Authentication
 // ===============================
 
 // Register User
 async function registerUser(fullName, email, phone, password) {
 
-    const { data, error } = await supabase
+    const { error } = await supabase
         .from("users")
         .insert([
             {
@@ -17,19 +17,22 @@ async function registerUser(fullName, email, phone, password) {
         ]);
 
     if (error) {
-        alert("Registration Failed!\n" + error.message);
-        return false;
+
+        alert(error.message);
+
+        return;
     }
 
     alert("Registration Successful!");
 
     window.location.href = "login.html";
 
-    return true;
 }
 
 
+
 // Login User
+
 async function loginUser(email, password) {
 
     const { data, error } = await supabase
@@ -40,33 +43,41 @@ async function loginUser(email, password) {
         .single();
 
     if (error || !data) {
+
         alert("Invalid Email or Password");
-        return false;
+
+        return;
     }
 
-    // Save logged in user
-    localStorage.setItem("user", JSON.stringify(data));
+    localStorage.setItem(
+        "currentUser",
+        JSON.stringify(data)
+    );
 
     window.location.href = "index.html";
 
-    return true;
 }
 
 
+
 // Logout
+
 function logoutUser() {
 
-    localStorage.removeItem("user");
+    localStorage.removeItem("currentUser");
 
     window.location.href = "login.html";
 
 }
 
 
+
 // Check Login
+
 function checkLogin() {
 
-    const user = localStorage.getItem("user");
+    const user =
+        localStorage.getItem("currentUser");
 
     if (!user) {
 
@@ -77,9 +88,13 @@ function checkLogin() {
 }
 
 
-// Get Logged In User
+
+// Get Current User
+
 function getCurrentUser() {
 
-    return JSON.parse(localStorage.getItem("user"));
+    return JSON.parse(
+        localStorage.getItem("currentUser")
+    );
 
 }
